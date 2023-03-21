543a9912960603fceed5fc2bd68b18ac2c02e014
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-21 14:57:00 -0700

    c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (num = 0 ; num <= 9 ; num++)
 		printf("%d", num);
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
 		putchar(alpha);
 	putchar('\n');
 	return (0);
