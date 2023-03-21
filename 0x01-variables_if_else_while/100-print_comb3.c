62dd2179df3e9cfa1b6a75c2025929c6c693813b
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-21 16:12:31 -0700

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 100; n++)
+	{
+		putchar('0' + (n/10));
+		putchar('0' + (n%10));
+
+		if (n != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar ('\n');
+	return (0);
+}
+
