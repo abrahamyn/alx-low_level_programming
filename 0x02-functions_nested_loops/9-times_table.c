e9fb43e5ae407b60ec8d295f0777d741c8923e3e
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-22 15:59:11 -0700

    function

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+/**
+ * times_table - check the code
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int n,l;
+	
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(48);
+		_putchar('\n');
+	}
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
+	for (n = 0; n < 10; n++)
+	{
+		l = n * 2;
+		_putchar(l);
+		_putchar('\n');
+	}
+	for (n = 0; n < 10; n++)
+        {
+                l = n * 3;
+                _putchar(l);
+                _putchar('\n');
+        }
+	for (n = 0; n < 10; n++)
+        {
+                l = n * 4;
+                _putchar(l);
+                _putchar('\n');
+        }
+	for (n = 0; n < 10; n++)
+        {
+                l = n * 5;
+                _putchar(l);
+                _putchar('\n');
+        }
+	for (n = 0; n < 10; n++)
+        {
+                l = n * 6;
+                _putchar(l);
+                _putchar('\n');
+        }
+	for (n = 0; n < 10; n++)
+        {
+                l = n * 7;
+                _putchar(l);
+                _putchar('\n');
+        }
+	for (n = 0; n < 10; n++)
+	{
+                l = n * 8;
+                _putchar(l);
+                _putchar('\n');
+        }
+	for (n = 0; n < 10; n++)
+        {
+                l = n * 9;
+                _putchar(l);
+                _putchar('\n');
+        }
+}
