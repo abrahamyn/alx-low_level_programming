336b73df8370271ea1ff8b10732496d44f2ba3b1
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-22 15:15:03 -0700

    function

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ *
+ * @n: integer parameter
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	if (n >= 0)
+		r = n % 10;
+	else
+		r = -1 * (n % 10);
+	return (r);
+}
