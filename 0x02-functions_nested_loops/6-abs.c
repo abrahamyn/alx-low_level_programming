f5866685a29667fa2319cc2edbdd97d185ae6607
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-22 14:44:22 -0700

    funciton

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - check the code
+ *
+ * @int: integer parameter
+ *
+ * Return: Always 0.
+ */
+int _abs(int)
+{
+	int r;
+	int n;
+
+	n = r * -1;
+	if (r < 0)
+		_putchar(n);
+	else
+		_putchar(r);
+}
