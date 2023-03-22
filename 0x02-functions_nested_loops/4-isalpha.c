4d9babea804437d6d45a865262e743b2ada9b7ed
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-22 14:25:49 -0700

    function

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - check the code.
+ *
+ * @c: integer parameter
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
