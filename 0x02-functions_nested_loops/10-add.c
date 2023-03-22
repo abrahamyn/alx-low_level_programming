febe51e1fbec8455be9d7d9fb5567f4c7a22c18d
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-22 16:56:01 -0700

    function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ *
+ * @x: integer parameter
+ *
+ * @y: integer parameter
+ *
+ * Return: Always 0.
+ */
+int add(int x, int y)
+{
+	int n;
+
+	n = x + y;
+	_putchar(n + '0');
+	return (0);
+}
