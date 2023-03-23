aaad85f99411bd5cc812cb8b30cf4a622c2380b1
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-23 15:21:41 -0700

    function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - check the code
+ *
+ * @a: integer parameter
+ *
+ * @b: integer parameter
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int n;
+
+	n = a * b;
+	return (n);
+}
