801acebeb78fb3e254c23e3628bdcee60fb3f564
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-26 12:14:03 -0700

    function

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ *
+ * @n: integer parameter
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
