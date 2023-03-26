69ad8834c3aece8317327d925f4bbecfdd0dd99a
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-26 14:30:21 -0700

    function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
@@ -20,6 +20,5 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
-	printf("Buzz");
 	return (0);
 }
