334496c6ab123561a4735cb36a1b4c8eb854ec2f
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-28 19:28:33 -0700

    pointer

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,8 +12,9 @@ void _puts(char *s)
 	int i;
 	int length;
 
-	for (length = 0; s[length] != '\0'; length++;
-
+	for (length = 0; s[length] != '\0'; length++
+	{
+	}
 	for (i = length - l; i >= 0; i--)
 	{
 		_putchar(s[i]);
