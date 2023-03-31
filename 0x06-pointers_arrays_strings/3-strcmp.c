15d2d00d4066b99dea3b6b04ab515e0c9077ecc3
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-31 10:46:41 -0700

    array

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strcmp - compares two strings
+ *
+ *@s1: string 1
+ *@s2: string 2
+ *Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, length1 = 0, length2 = 0, n = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		length1++;
+	for (j = 0; s2[j] != '\0'; i++)
+		length2++;
+	if (length1 > length2)
+		n++;
+	else if (length1 < length1)
+		n--;
+	else
+		n;
+	return (n);
+}
