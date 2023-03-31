32bfa8e6747cf9fface4d78fd43d133bc2d48c1f
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-31 13:49:11 -0700

    array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,8 +9,14 @@
  */
 void reverse_array(int *a, int n)
 {
-	for (int i = n - 1; i >= 0; i--)
+	int i = 0, tmp, j = n - 1; 
+	
+	while (i < n / 2)
 	{
-		printf("%d", a[i]);
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		j--;
+		i++;
 	}
 }
