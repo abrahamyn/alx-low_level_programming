e145340813301d9344dd9a5aa2d1456f29aea229
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-31 02:05:38 -0700

    array

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *_strcat - appends dest to src
+ *
+ * @dest: pointer
+ * @src: pointer
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j, lenght = 0;
+
+	dest = d[];
+	src = s[];
+
+	for (i = 0; d[i] != '\0'; i++)
+	{
+		length++;
+	}
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		d[length] = s[j];
+		length++;
+	}
+	dest = d[length];
+	return (dest);
+}
