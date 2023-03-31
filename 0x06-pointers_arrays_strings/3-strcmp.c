be3a95cf4c252f66023a47c32585ee6722ee77bd
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-31 12:53:06 -0700

    array

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,14 @@
  *@s2: string 2
  *Return: Always 0.
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, n = 0;
+	int i, n = 0;
 
 	for (i = 0; s1[i] != 0 && s2[i] != 0 && n == 0; i++)
 	{
-		n = s1[i] - s2[j];
+		n = s1[i] - s2[i];
 	}
 	return (n);
 }
