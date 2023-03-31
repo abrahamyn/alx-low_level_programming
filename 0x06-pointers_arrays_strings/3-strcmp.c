523b60e66fccfd90712f3ae5b10fc5245985395a
Abraham Negussie <yesigatabraham@gmail.com>
2023-03-31 10:51:11 -0700

    array

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		length1++;
-	for (j = 0; s2[j] != '\0'; i++)
+	for (j = 0; s2[j] != '\0'; j++)
 		length2++;
 	if (length1 > length2)
 		n++;
