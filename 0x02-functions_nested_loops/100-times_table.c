7a3fd3fe71b5a20867c95cb78ae979fc1353e242
Abraham Negussie <yesigatabraham@gmail.com>
2023-04-02 09:07:22 -0700

    function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,41 @@
 #include "main.h"
 /**
+ *print_number - print a number using _putchar.
+ *@n: the number to be printed.
+ */
+
+void print_number(int n)
+{
+	unsigned int i = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		while ((n / i) >= 10)
+			i *= 10;
+
+		while (i > 0)
+		{
+			_putchar((n / i) + '0');
+			n %= i;
+			i /= 10;
+		}
+	}
+}
+/**
  * print_times_table -prints tables
  *
  * @n: integer parameter
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, x = 0;
 
 	if (n < 0 || n > 15)
 	{
@@ -17,10 +46,16 @@ void print_times_table(int n)
 		{
 			for (j = 0; j < n + 1; j++)
 			{
-				_putchar((j * i) + '0');
+				x = i * j;
+				print_number(x);
 				if (!(j == n))
 				{
 					_putchar(44);
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
 					_putchar(' ');
 					_putchar(' ');
 				}
