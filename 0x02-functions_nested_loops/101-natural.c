673246b4b5c5bc01318c892ce7d37bde06307d4f
Abraham Negussie <yesigatabraham@gmail.com>
2023-04-02 10:13:15 -0700

    function

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0.
  */
-int main()
+int main(void)
 {
 	int i, sum = 0;
 
 	for (i = 1; i < 1024; i++)
 	{
-		if (i % 3 == 0|| i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum += i;
 		}
