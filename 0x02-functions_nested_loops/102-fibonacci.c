f31ae58861a024afae15c1cd972934ac306c7986
Abraham Negussie <yesigatabraham@gmail.com>
2023-04-02 10:54:13 -0700

    function

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints 50 fibonacci numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 1, j = 2, n, x = 0;
+
+	printf("%d, %d, ", i,j);
+	for (n = 0; n < 50; n++)
+	{
+		x = i + j;
+		i = j;
+		j = x;
+		printf("%d, ", x);
+	}
+	return (0);
+}
