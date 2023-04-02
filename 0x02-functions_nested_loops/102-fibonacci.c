f5f11bcae71ddb318262dc1abb1d53cd071b9cda
Abraham Negussie <yesigatabraham@gmail.com>
2023-04-02 11:00:55 -0700

    function

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int i = 1, j = 2, n, x = 0;
 
-	printf("%d, %d, ", i,j);
-	for (n = 0; n < 50; n++)
+	printf("%d, %d", i, j);
+	for (n = 0; n < 48; n++)
 	{
 		x = i + j;
 		i = j;
 		j = x;
-		printf("%d, ", x);
+		printf(", %d", x);
 	}
 	return (0);
 }
