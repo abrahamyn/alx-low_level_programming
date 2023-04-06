38c1ea3b1884881a49d8f77d119741a07b9ce9dc
abrahamyn <125731643+abrahamyn@users.noreply.github.com>
2023-04-06 04:10:00 +0300

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,8 @@
 /**
  * _pow_recursion - prints a string in reverse
  *
- * @n: character parameter
+ * @x: integer parameter
+ * @y: integer parameter
  * Return: Always 0.
  */
 int _pow_recursion(int x, int y)
