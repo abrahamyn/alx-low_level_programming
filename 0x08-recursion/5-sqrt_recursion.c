f17c2a2ba58a0bf2478d8da1f774952ffb071c88
abrahamyn <125731643+abrahamyn@users.noreply.github.com>
2023-04-06 23:45:02 +0300

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * sqrt2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int sqrt2(int a, int b)
+{
+if (b * b == a)
+return b;
+if (b * b > a)
+return (-1);
+else
+return (sqrt2(a, b + 1));
+}
+/**
  * _sqrt_recursion - check the code
  *
  * @n: integer parameter
@@ -8,12 +25,5 @@
  */
 int _sqrt_recursion(int n)
 {
-int x = n;
-  if ((x * x) == n)
-    return x;
-  else
-  {
-    x--;
-    return _sqrt_recursion(n);
-  }
+return (sqrt2(n, 1);
 }
