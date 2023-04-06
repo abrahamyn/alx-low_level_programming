0aab401d631b870b7cc0ed3dfd382d07b84a1ce7
abrahamyn <125731643+abrahamyn@users.noreply.github.com>
2023-04-06 03:24:25 +0300

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,15 @@
-kdhfieua
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints a string in reverse
+ *
+ * @s: character parameter
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+  if (*s != 0)
+  {
+    return(1 + _strlen_recursion(s + 1));
+  }
+}
