05697a7256a8e76d32d0dfdc8ffe91ce27f409d5
abrahamyn <125731643+abrahamyn@users.noreply.github.com>
2023-04-10 02:24:48 +0400

    arg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,6 @@
 /**
 * main - prints the name of the program
 *
-<<<<<<< HEAD
 * @argc: the number of arguments
 * @argv: array of arguments
 * Return: Always 0.
@@ -12,14 +11,5 @@
 int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-=======
-* @argc: number of arguments
-* @argv: array of arguments
-* Return: Always 0.
-*/
-int main(int __attribute__((unused)) argc, char **argv)
-{
-	printf("%s\n", *argv);
->>>>>>> acfc07ce914ae0729fcccea6c7a1d986c929e624
 	return (0);
 }
