eb83e515c68a93875d552fa1b092ae7e595c8a15
abrahamyn <125731643+abrahamyn@users.noreply.github.com>
2023-04-10 01:12:56 +0400

    arg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,18 @@
 * @argv: array of arguments
 * Return: Always 0.
 */
-int main(int argc, char **argv)
+int main(int argc, float **argv)
 {
 int i = argv[1] * argv[2];
 
 if (argc != 3)
+{
 printf("Error");
+return (1);
+}
 else
+{
 printf("%d\n", i);
 return (0);
+}
 }
